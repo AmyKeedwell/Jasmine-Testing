e8aabc61057d9f4976488a0162141e3bfc47f30b
AmyKeedwell <amii1595@live.co.uk>
2020-07-17 11:14:38 +0000

    Programming Defensively

--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -6,5 +6,8 @@ describe("calculator", function (){
         it("should return 26", function(){
             expect(addition(7, 19)).toBe(26);
         })
+        it("should return an error if we dont't supply two numbers", function(){
+            expect(addition("Hitchhikers", "Guide")).toBe("Error!");
+        })
     });
 });
